cf38585dc3c0353c9652d4ca172364f0ec6cdd2a
matthew <matthew@foursquare.com>
2010-12-04 13:23:32 -0500

    issue filled out more

--- src/mlproject/models/Issue.java
+++ src/mlproject/models/Issue.java
@@ -4,9 +4,24 @@ package mlproject.models;
 
 public class Issue{
 	
-	public Issue(String csv){
+	HashMap<String, String> issueData;
+	String mImagePath;
+	
+	public Issue(String csvFields, String csv, String imagePath) throws ArgumentException{
+		this.mImagePath = imagePath;
+		String[] titles = csvFields.split(",\\s*")
+		String[] data = csv.split(",\\s*")
+		if(titles.size != data.size) throw new ArgumentException("# titles doesn't match # of fields");
 		
-		
+		for(int i = 0; i < titles.size; i++){
+			String title = titles[i];
+			String d = data[i];
+			issueData.put(title, d);
+		}
+	}
+	
+	public String get(String t){
+		return issueData.get(t);
 	}
 	
 	
