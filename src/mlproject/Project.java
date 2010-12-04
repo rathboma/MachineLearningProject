358f254bb6eb417df9da0b737d15196c2f2a8f92
matthew <matthew@foursquare.com>
2010-12-04 13:51:07 -0500

    email

--- src/mlproject/Project.java
+++ src/mlproject/Project.java
@@ -1,12 +1,15 @@
 package mlproject;
 
-
+import mlproject.models.*;
+import mlproject.predictors.*;
+import mlproject.testing.*;
 
 
 
 public class Project {
 	
-	public static void main(){
+	public static void main(String[] args){
+		System.out.println("hello!");
 		
 	}
 	
--- src/mlproject/models/Issue.java
+++ src/mlproject/models/Issue.java
@@ -1,5 +1,7 @@
 package mlproject.models;
 
+import java.util.HashMap;
+
 
 
 public class Issue{
@@ -7,13 +9,13 @@ public class Issue{
 	HashMap<String, String> issueData;
 	String mImagePath;
 	
-	public Issue(String csvFields, String csv, String imagePath) throws ArgumentException{
+	public Issue(String csvFields, String csv, String imagePath) throws Exception{
 		this.mImagePath = imagePath;
-		String[] titles = csvFields.split(",\\s*")
-		String[] data = csv.split(",\\s*")
-		if(titles.size != data.size) throw new ArgumentException("# titles doesn't match # of fields");
+		String[] titles = csvFields.split(",\\s*");
+		String[] data = csv.split(",\\s*");
+		if(titles.length != data.length) throw new Exception("# titles doesn't match # of fields");
 		
-		for(int i = 0; i < titles.size; i++){
+		for(int i = 0; i < titles.length; i++){
 			String title = titles[i];
 			String d = data[i];
 			issueData.put(title, d);
